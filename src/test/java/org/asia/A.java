4cf8bf4a0ae9419153f3fee5da44a37aa5882573
aravinthan11 <aravinthan1412111@gmail.com>
2020-02-24 18:16:14 +0530

    I have done the coding for Add to cart functionality

--- src/test/java/org/asia/A.java
+++ src/test/java/org/asia/A.java
@@ -8,4 +8,7 @@ System.out.println("THE FIRST DAY");
 private void login() {
 System.out.println("Login Successfull");
 }
+private void Addtocart() {
+System.out.println("product added to the cart");
+}
 }
